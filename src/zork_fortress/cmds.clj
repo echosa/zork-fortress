236b08ee1331ce11f53c89e59cc0d268eef3677f
Brian Zwahr <echosa@gmail.com>
2014-08-05 16:53:42 -0500

    Refactored run-cmd quite a bit.

--- src/zork_fortress/cmds.clj
+++ src/zork_fortress/cmds.clj
@@ -9,23 +9,34 @@
   [game]
   "You see nothing.")
 
+(t/ann get-new-last-turn [t2/Game t2/Command -> t2/Turn])
+(defn get-new-last-turn
+  "Generate a new last turn entry for the game."
+  [game command]
+  (let [args (when (:args command) (:args command))]
+    (merge {:command command}
+           (condp = (:trigger command)
+             'look {:response (look-cmd game)}
+             'history {:response (if (nil? args)
+                                   (history-cmd game)
+                                   (history-cmd game :args args))}
+             {:response "Invalid command." :invalid true}))))
+
+(t/ann get-new-turn-history [t2/Game -> (t/Vec (t/Option t2/Turn))])
+(defn get-new-turn-history
+  "Generate a new turn history for the game."
+  [game]
+  (let [last-turn-for-history (get-last-turn-for-history game)]
+    (if last-turn-for-history
+      (conj (:turn-history game) last-turn-for-history)
+      (:turn-history game))))
+
 (t/ann run-cmd [t2/Game t2/Command -> t2/Game])
 (defn run-cmd
   "Run the given command."
   [game command]
-  (let [trigger (:trigger command)
-        args (when (:args command) (:args command))
-        last-turn-for-history (get-last-turn-for-history game)]
-    (merge game 
-           {:last-turn (merge {:command command}
-                              (condp = trigger
-                                'look {:response (look-cmd game)}
-                                'history {:response (if (nil? args)
-                                                      (history-cmd game)
-                                                      (history-cmd game :args args))}
-                                {:response "Invalid command." :invalid true}))
-            :turn-history (if last-turn-for-history
-                            (conj (:turn-history game) last-turn-for-history)
-                            (:turn-history game))})))
+  (merge game 
+         {:last-turn (get-new-last-turn game command)
+          :turn-history (get-new-turn-history game)}))
 
 
