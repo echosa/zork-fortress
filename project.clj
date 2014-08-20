8a2119205842a0388d840194b9faa2151abea2c1
Brian Zwahr <echosa@gmail.com>
2014-08-20 11:08:12 -0500

    Set version to 0.0.1.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject zork-fortress "0.1.0-SNAPSHOT"
+(defproject zork-fortress "0.0.1"
   :description "A text world-building adventure!"
   :url "http://echosa.github.io"
   :license {:name "Eclipse Public License"
